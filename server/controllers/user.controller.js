2d2fa25611fe08241144c3108123f7de64eaa355
SohanDee <sohandeemantha@gmail.com>
2024-07-31 23:36:47 +0530

    create signup api and routes

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,5 @@
+const test = (req,res)=>{
+    res.send("Hello");
+}
+
+module.exports = test
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const routes = require('./routes/index')
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -14,3 +16,7 @@ const App = express();
 App.listen(3001, ()=>{
     console.log("App is running on port 3001")
 })
+
+App.use(express.json())
+
+App.use('/', routes)

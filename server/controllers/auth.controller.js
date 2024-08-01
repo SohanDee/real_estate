49a37e4e5a0cebb327bc1fd0912e9ae019fc14bf
SohanDee <sohandeemantha@gmail.com>
2024-08-01 19:56:11 +0530

    create sign in api routes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/user.model.js')
 const bcryptjs = require("bcryptjs");
-
+const errorHandler = require('../utils/error.js')
+const jwt = require('jsonwebtoken')
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,4 +15,23 @@ const signup = async (req, res, next) => {
   }
 };
 
-module.exports = { signup };
+const signin = async (req, res, next) => {
+  try {
+    const {email, password} = req.body;
+    const validUser = await User.findOne({email:email});
+    if(!validUser){
+     return next(errorHandler(404, "User not found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword){
+      return (next(errorHandler(401, "Wrong Credentials!")));
+    }
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+    const {password: pw, ...rest} = validUser._doc
+    res.cookie("access_token", token, {httpOnly:true}).status(200).json(rest);
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = { signup, signin};

e5463b2c4bfedbfe93533de05716c46c34125786
SohanDee <sohandeemantha@gmail.com>
2024-08-03 23:38:22 +0530

    create store and user slice using redux

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user : null,
+    loading: false,
+    error: null
+}
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        signInStart: (state) => {
+            state.loading = true;
+        },
+        signInSuccess: (state, action) => {
+            state.user = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        signInFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        }
+    }
+})
+
+export const { signInStart, signInSuccess, signInFailure} = userSlice.actions;
+
+export default userSlice.reducer;
